2c99bff3c53faef6558cb88df23eb1c16c22b191
Mori Airuta <mori.airuta@gmail.com>
2021-08-26 22:21:46 +0200

    add ghcup init to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,3 +12,9 @@ starship init fish | source
 # Set up pyenv
 status is-interactive; and pyenv init --path | source
 pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
+
+# Set up ghcup
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+fish_add_path $HOME/.cabal/bin
+fish_add_path $HOME/.ghcup/bin
